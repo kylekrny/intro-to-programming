9ea8737ea50f41d627ef2c037a7bf18abe874593
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-10-13 10:50:03 -0400

    unit 1 completed

---
+++ unit-1/python.py
@@ -0,0 +1,87 @@
+print("Hello World")
+
+# Remainder of 20 
+# print (20%6)
+
+# Exponent **
+# print(4*2**2)
+
+# print("10" + "10")
+
+# myString = 'Python is great\n'
+# lines = 4
+# result = 'lines' + lines* (myString)
+# print(myString.swapcase())
+
+
+# a = 5
+# b = 15
+# c = 10
+# d = 0
+# if a > b:
+#  d = 4
+# elif b > c:
+#  d = 5
+# else:
+#  d = 6
+# print(d)
+
+
+# grade = 71
+# if grade > 90:
+#   print("You got an A")
+# elif grade > 80:
+#   print("You got a B")
+# elif grade > 70:
+#   print("You got a C")
+# elif grade > 60:
+#   print("You got a D")
+# elif grade <= 60:
+#   print("You got a F")
+# print("We are done!")
+
+# myVar1 = '3'
+# myVar2 = '2'
+# lines = int(myVar1) + int(myVar2)
+# myStr = myVar2 + ' ' + myVar1 + '\n'
+# result = lines * ('lines' + myStr)
+# print(result)
+
+# a = 1
+# b = 2
+# c = 3
+# d = 0
+# if a > b: 
+#   d = 4
+# else: 
+#   if a > c: 
+#     d = 5
+# print(d)
+
+# temp_Fahrenheit = 104
+# if temp_Fahrenheit < 50: 
+#   print('It is cold outside')
+# elif temp_Fahrenheit > 80: 
+#   print('It is hot outside')
+# elif temp_Fahrenheit > 90: 
+#   print('It is very hot outside')
+# else: 
+#   print('It must be a nice day')
+
+# price = 12
+# quantity = 2
+# amount = price*quantity
+# if amount > 100:
+#     if amount > 500:
+#         print("Amount is greater than 300")
+#     else:
+#         if amount < 500 and amount > 400:
+#              print("Amount is")
+#         elif amount < 500 and amount > 300:
+#              print("Amount is between 300 and 500")
+#         else:
+#              print("Amount is between 200 and 500")
+# elif amount == 100:
+#      print("Amount is 100")
+# else:
+#      print("Amount is less than 100")
